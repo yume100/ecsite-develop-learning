161414b06df11d0f72e3de30b7f42ec7824daf48
daiki-h <imisin2008@gmail.com>
2018-06-24 11:50:09 +0900

    DevelopLearning Starter

---
+++ src/main/java/com/example/ecsitedeveloplearning/EcsiteDevelopLearningApplication.java
@@ -0,0 +1,12 @@
+package com.example.ecsitedeveloplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsiteDevelopLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsiteDevelopLearningApplication.class, args);
+	}
+}
