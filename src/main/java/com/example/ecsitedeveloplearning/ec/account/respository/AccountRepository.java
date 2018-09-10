b96acab90291b3005c715ddaf9a19ca8b21ee0d0
toto <toto@DESKTOP-7U2GSSI>
2018-09-10 12:46:23 +0900

    環境構築 package構成

---
+++ src/main/java/com/example/ecsitedeveloplearning/ec/account/respository/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.example.ecsitedeveloplearning.ec.account.respository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface AccountRepository {
+
+}
---
+++ src/main/java/com/example/ecsitedeveloplearning/ec/account/web/AccountController.java
@@ -0,0 +1,25 @@
+package com.example.ecsitedeveloplearning.ec.account.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.ecsitedeveloplearning.ec.account.service.AccountService;
+
+
+@Controller
+@RequestMapping(path="/account")
+public class AccountController {
+
+	@Autowired
+	private AccountService accountService;
+
+	// Top Page
+	@GetMapping("/login")
+	public String viewLogin() {
+		System.out.println("viewLogin");
+		return "account/login";
+	}
+
+}
